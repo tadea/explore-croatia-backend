1187d92cb25fd5b75450d103c61422e1b09184d3
Tadea <tsimunoviczg@gmail.com>
2019-08-17 15:43:53 -0400

    add attribute for UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,15 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :firstname, :lastname, :name
-  has_many :fav_beaches
+  #has_many :fav_beaches
 
+  attribute :fav_beaches do |user|
+    user.fav_beaches.map do |fav_beach|
+      {
+        name: fav_beach.beach.name,
+        location: fav_beach.beach.location,
+        beach_type: fav_beach.beach.beach_type
+      }
+    end
+  end
 end
