2917dbf2ddfa2d50035620015fc8ad2d23c66b4a
Tadea <tsimunoviczg@gmail.com>
2019-09-08 14:01:14 -0400

    add current_user and logged_in? methods, add cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::API
-
+    include ::ActionController::Cookies
     #mocked version of "being logged in"
     def current_user
-        User.first
+        User.find_by(id: session[:user_id])
     end
-
+    
     def logged_in?
         !!current_user
     end
-
 end
