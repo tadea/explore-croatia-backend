cc8b67debaa4efb81321dc8f206644bfe2fffe7d
Tadea <tsimunoviczg@gmail.com>
2019-09-16 23:38:27 -0400

    add validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
     
     has_many :fav_beaches
     has_many :beaches, through: :fav_beaches
+
+    validates :firstname, :lastname, :name, presence: true
+    validates :name, uniqueness: true
 end
