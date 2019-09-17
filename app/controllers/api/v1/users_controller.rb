a3fc4069408e99a893525c8f02489a966e8764b1
Tadea <tsimunoviczg@gmail.com>
2019-09-16 23:38:07 -0400

    update create action in userController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,54 @@
 class Api::V1::UsersController < ApplicationController
-    before_action :find_user
+    before_action :find_user, only: [:show, :update, :destroy]
     
-    def show
-        user_json = UserSerializer.new(@user).serialized_json
-        render json: user_json
-    end
+# GET /users
+def index
+    @users = User.all
+    render json: UserSerializer.new(@users)
+  end
 
-    private
-    def find_user
-        @user = User.find(params[:id])
-    end
+# GET /users/1
+def show
+    user_json = UserSerializer.new(@user).serialized_json
+    render json: user_json
+end
 
-    def user_params
+# POST /users
+def create
+    @user = User.new(user_params)
+
+   if @user.save
+     session[:user_id] = @user.id
+     render json: UserSerializer.new(@user), status: :created
+   else
+     resp = {
+       error: @user.errors.full_messages.to_sentence
+     }
+     render json: resp, status: :unprocessable_entity
+   end
+end
+
+# PATCH/PUT /users/1
+def update
+  if @user.update(user_params)
+    render json: @user
+  else
+    render json: @user.errors, status: :unprocessable_entity
+  end
+end
+
+# DELETE /users/1
+def destroy
+  @user.destroy
+end
+
+
+ private
+ def find_user
+     @user = User.find(params[:id])
+ end
+
+ def user_params
         params.require(:user).permit(:firstname, :lastname, :name, :password)
     end
 end
